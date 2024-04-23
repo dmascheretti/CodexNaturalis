f103bffbfaa7157214cddd152d7ea82ef1a83076
dmascheretti <167296926+dmascheretti@users.noreply.github.com>
2024-04-23 16:25:54 +0200

    b
    b

--- src/progetto/Carta.java
+++ src/progetto/Carta.java
@@ -10,7 +10,6 @@ public class Carta {
 	private String risorse1,risorse2,risorse3,risorse4;
 	private String risorse1r,risorse2r;
 	private Colore_carta colore;
-	private int a;
 
 	
 	/**Creates a gold card
