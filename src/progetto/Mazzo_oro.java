493aa44a03ac95e76e89946a726cf19f10e65b5c
dmascheretti <167296926+dmascheretti@users.noreply.github.com>
2024-04-29 10:04:32 +0200

    Update Mazzo_oro.java

--- src/progetto/Mazzo_oro.java
+++ src/progetto/Mazzo_oro.java
@@ -21,44 +21,7 @@ public Mazzo_oro(){
 	mazzo_oro.add(carta_oro2);
 	mazzo_oro.add(carta_oro3);
 
-/*
-	
-	mazzo_oro.add(Carta_oro6);
-	mazzo_oro.add(Carta_oro7);
-	mazzo_oro.add(Carta_oro8);
-	mazzo_oro.add(Carta_oro9);
-	mazzo_oro.add(Carta_oro10);
-	mazzo_oro.add(Carta_oro11);
-	mazzo_oro.add(Carta_oro12);
-	mazzo_oro.add(Carta_oro13);
-	mazzo_oro.add(Carta_oro14);
-	mazzo_oro.add(Carta_oro15);
-	mazzo_oro.add(Carta_oro16);
-	mazzo_oro.add(Carta_oro17);
-	mazzo_oro.add(Carta_oro18);
-	mazzo_oro.add(Carta_oro19);
-	mazzo_oro.add(Carta_oro20);
-	mazzo_oro.add(Carta_oro21);
-	mazzo_oro.add(Carta_oro22);
-	mazzo_oro.add(Carta_oro23);
-	mazzo_oro.add(Carta_oro24);
-	mazzo_oro.add(Carta_oro25);
-	mazzo_oro.add(Carta_oro26);
-	mazzo_oro.add(Carta_oro27);
-	mazzo_oro.add(Carta_oro28);
-	mazzo_oro.add(Carta_oro29);
-	mazzo_oro.add(Carta_oro30);
-	mazzo_oro.add(Carta_oro31);
-	mazzo_oro.add(Carta_oro32);
-	mazzo_oro.add(Carta_oro33);
-	mazzo_oro.add(Carta_oro34);
-	mazzo_oro.add(Carta_oro35);
-	mazzo_oro.add(Carta_oro36);
-	mazzo_oro.add(Carta_oro37);
-	mazzo_oro.add(Carta_oro38);
-	mazzo_oro.add(Carta_oro39);
-	mazzo_oro.add(Carta_oro40);
-	*/
+
 	
 }
 
