6b59e3e620d5bf8e361fe62cf340bc6b52f85852
dmascheretti <167296926+dmascheretti@users.noreply.github.com>
2024-04-30 14:18:41 +0200

    Update Mazzo_oro.java

--- src/progetto/Mazzo_oro.java
+++ src/progetto/Mazzo_oro.java
@@ -9,44 +9,64 @@ public class Mazzo_oro extends Mazzo{
 private ArrayList<Carta_oro> mazzo_oro = new ArrayList<Carta_oro>();
 private Carta_oro carta_oro1=new Carta_oro("",Regno.NULL.toString(),"","",
 		2,"VVVF", Colore_carta.VERDE,Regno.VEGETALE.toString(),80);
+
 private Carta_oro carta_oro2=new Carta_oro(Regno.NULL.toString(),"","",Regno.PERGAMENA.toString(),
 		1,"AAV", Colore_carta.BLU,Regno.ANIMALE.toString(),81);
+
 private Carta_oro carta_oro3=new Carta_oro("",Regno.NULL.toString(),Regno.PIUMA.toString(),"",
 		1,"AAF", Colore_carta.BLU, Regno.ANIMALE.toString(),82);
+
 private Carta_oro carta_oro4=new Carta_oro(Regno.NULL.toString(),"","","",
 		2,"AAAV", Colore_carta.BLU, Regno.ANIMALE.toString(),83);
+
 private Carta_oro carta_oro5=new Carta_oro(Regno.NULL.toString(),"","",Regno.NULL.toString(),
 		5,"AAAAA", Colore_carta.BLU, Regno.ANIMALE.toString(),84);
+
 private Carta_oro carta_oro6=new Carta_oro("","",Regno.NULL.toString(),"",
 		2,"AAAI", Colore_carta.BLU, Regno.ANIMALE.toString(),85);
+
 private Carta_oro carta_oro7=new Carta_oro("",Regno.NULL.toString(),"","",
 		2,"AAAF", Colore_carta.BLU, Regno.ANIMALE.toString(),86);
+
 private Carta_oro carta_oro8=new Carta_oro(Regno.NULL.toString(),"",Regno.NULL.toString(),Regno.PIUMA.toString(),
 		3,"AAA", Colore_carta.BLU, Regno.ANIMALE.toString(),87);
+
 private Carta_oro carta_oro9=new Carta_oro("",Regno.NULL.toString(),Regno.PERGAMENA.toString(),"",
 		3,"AAA", Colore_carta.BLU, Regno.ANIMALE.toString(),88);
+
 private Carta_oro carta_oro10=new Carta_oro("",Regno.PERGAMENA.toString(),"","",
 		3,"AAA", Colore_carta.BLU, Regno.ANIMALE.toString(),89);
+
 private Carta_oro carta_oro11=new Carta_oro("","",Regno.NULL.toString(),Regno.NULL.toString(),
 		5,"VVVVV", Colore_carta.VERDE, Regno.VEGETALE.toString(),90);
+
 private Carta_oro carta_oro12=new Carta_oro(Regno.PIUMA.toString(),"","",Regno.NULL.toString(),
 		5,"VVI", Colore_carta.VERDE, Regno.VEGETALE.toString(),91);
+
 private Carta_oro carta_oro13=new Carta_oro(Regno.PIUMA.toString(),"",Regno.NULL.toString(),Regno.NULL.toString(),
 		3,"FFF", Colore_carta.ROSSO, Regno.INSETTI.toString(),92);
+
 private Carta_oro carta_oro14=new Carta_oro(Regno.NULL.toString(),Regno.PERGAMENA.toString(),Regno.NULL.toString(),"",
 		3,"FFF", Colore_carta.ROSSO, Regno.INSETTI.toString(),93);
+
 private Carta_oro carta_oro15=new Carta_oro("",Regno.NULL.toString(),Regno.POZIONE.toString(),Regno.NULL.toString(),
 		3,"FFF", Colore_carta.ROSSO, Regno.INSETTI.toString(),94);
+
 private Carta_oro carta_oro16=new Carta_oro("",Regno.NULL.toString(),"",Regno.NULL.toString(),
 		5,"FFFFF", Colore_carta.ROSSO, Regno.INSETTI.toString(),95);
+
 private Carta_oro carta_oro17=new Carta_oro("","","",Regno.NULL.toString(),
 		2,"FFFV", Colore_carta.ROSSO, Regno.INSETTI.toString(),96);
+
 private Carta_oro carta_oro18=new Carta_oro(Regno.PERGAMENA.toString(),"","",Regno.NULL.toString(),
 		1,"FFI", Colore_carta.ROSSO, Regno.INSETTI.toString(),97);
+
 private Carta_oro carta_oro19=new Carta_oro("",Regno.NULL.toString(),"","",
 		2,"FFFI", Colore_carta.ROSSO, Regno.INSETTI.toString(),98);
+
 private Carta_oro carta_oro20=new Carta_oro("","",Regno.NULL.toString(),"",
 		2,"FFFA", Colore_carta.ROSSO, Regno.INSETTI.toString(),99);
+
 private Carta_oro carta_oro21=new Carta_oro(Regno.NULL.toString(),"","",Regno.PIUMA.toString(),
 		1,"FFA", Colore_carta.ROSSO, Regno.INSETTI.toString(),100);
 
