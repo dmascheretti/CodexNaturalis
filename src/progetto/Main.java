3f75ad57aadf109f557efec8b76c4168a853479e
Benedettaaaa <118062743+Benedettaaaa@users.noreply.github.com>
2024-05-02 11:07:06 +0200

    Adding Main class
    Adding exception to Main class

--- src/progetto/Main.java
+++ src/progetto/Main.java
@@ -1,8 +1,10 @@
 package progetto;
 
+import java.io.IOException;
+
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Gioco codex=new Gioco();
 		codex.Gioca();
 		
